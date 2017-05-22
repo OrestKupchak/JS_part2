45a004908331de79c14a864cbd217187150236c3
OrestKupchak <orest.kupchak@gmail.com>
2017-05-22 15:24:10 +0300

    Done!

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@ document.getElementById('btnEven').addEventListener('click', evenNums, false)
  function evenNums(start, end){
 var start = parseInt(document.getElementById('startEven').value);
   var end = parseInt(document.getElementById('endEven').value);
-  debugger;
+  //debugger;
      if (start < end) {
     var result = [];
     for (var i = start; i <= end; i++) {if (i % 2 === 0) { result.push(i); } }
@@ -57,7 +57,7 @@ for (var i = start; i >= end; i--) { if ( i % 2 === 0) { result.unshift(i); }  }
 
 document.getElementById('btnOdd').addEventListener('click', oddNums, false) 
 
-  debugger;
+  //debugger;
  function oddNums (start, end){
 var start = parseInt(document.getElementById('startOdd').value);
   var end = parseInt(document.getElementById('endOdd').value);
@@ -83,8 +83,28 @@ while (i <= end) {        if ( i % 2 === 1) { result.push(i); }   i++  }
 
 /*----------------------------------------------------------------------------------*/
 
-/*Script for task 5-6: "Prompt" him to death (while / do...while)*/
+/*Kata 5-6: Display all number till stop condition (for && while)*/
+document.getElementById('btnStop').addEventListener('click', displayNumbers, false) 
 
+function displayNumbers (start, end, birth){
+var start = parseInt(document.getElementById('startIter').value);
+var end = parseInt(document.getElementById('endIter').value);
+var birth = parseInt(document.getElementById('stopIter').value);
+//  debugger;
+
+var result = [];
+if (start < -100 || start > 100) {var result = "Let's count only between -100 and 100"}
+else if (end < -100 || end > 100) { var result = "Let's count only between -100 and 100"}
+else if (birth < 1 || birth > 12) {var result = "There's only twelve months, mate :)"}
+else if ((end - start) < birth ) { var result = "Nope, can not iterate more times, than we have in range"}
+else {
+   result = []; for (var i = 0; i < birth; i++) { result.push(start++); }
+
+           }
+document.getElementById('iterationResults').innerHTML =  result; 
+}
+
+/*
 document.getElementById('btnStop').addEventListener('click', displayNumbers, false) 
 
 
@@ -133,6 +153,7 @@ else {
            }
 document.getElementById('iterationResults').innerHTML =  result; 
 }
+*/
 
 /*----------------------------------------------------------------------------------*/
 
@@ -147,15 +168,18 @@ var input = prompt('Enter number greater than 100');
 var spacesPattern = /^[\s]+$/ ;  // RegEx pattern to review if text contains no chars, only spaces
 
 if (spacesPattern.test(input)) {
-input = prompt ('You entered nothing, only spaces. Try again');
-}
+var input = prompt ('You entered nothing, only spaces. Try again');
+    }
 else {
-debugger;
-  while (isNaN(input) || typeof(parseInt(input)) !== 'number' || input.length === 0 || (spacesPattern.test(input))) { 
-    input = prompt('What\'s that? Give me some numbers, bro!')
-  } 
+//  debugger;
+    while (isNaN(input) || typeof(parseInt(input)) !== 'number' || input.length === 0 || (spacesPattern.test(input))) { 
+     var input = prompt('What\'s that? Give me some numbers, bro!')
+    } 
+
+    
   while (parseInt(input) < 100) {
-   input = prompt('Your number is too small, enter a new one');
+    if (isNaN(input)) {prompt('What\'s that? Give me some numbers, bro!')}
+   var input = prompt('Your number is too small, enter a new one');
       }
  }
 document.getElementById('promptResult').innerHTML =  result;
@@ -168,14 +192,20 @@ alert(`Your result is ${input}`)
 
 document.getElementById('createHill').addEventListener('click', hashes, false) 
 
-
+//debugger;
  function hashes (num){
    var num = parseInt(document.getElementById('bottom').value);
     var result = '';
-    for (var i = 0; i <= num; i++){
-       result += '#'+'\n';
+if (num > 25) {
+  result = 'Nope, thats too much. Don\'t go more than 25';
+       document.getElementById('hashHill').innerHTML = result;
+} else {
+
+    for (var i = 1; i <= num; i++){
+        result += '#'.repeat(i) + '\r\n';
        document.getElementById('hashHill').innerHTML = result;
       }
+  }
 }
 
 /*----------------------------------------------------------------------------------*/
